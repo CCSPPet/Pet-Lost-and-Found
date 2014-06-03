23743834d41a74d5080cf071f18ddf96a5d511ad
John Wu <john0000wu@gmail.com>
2014-06-03 22:47:03 +0800

    add pages

--- public/js/main.js
+++ public/js/main.js
@@ -23,6 +23,31 @@ $(document).ready(function(){
 		return false;
 	});
 	
+
+//張貼    
+$('#post').click(function(){
+    $('#form').removeClass('hide')
+    $('#blackcover').removeClass('hide')
+    console.log("show")
+});
+
+//取消    
+$('.cancel').click(function(){
+    $('#form').addClass('hide')
+    $('#blackcover').addClass('hide')
+    console.log('hide')
+});
+
+//增加懸賞    
+  $('#rewardBtn').click(function(){
+      $('<div class="form-group"><label for="inputEmail" class="col-lg-2 control-label">懸賞</label><div class="col-lg-10"><input type="text" class="form-control" id="inputEmail" placeholder="金錢或物品" ></div></div>').appendTo($('fieldset')).find('input').focus()
+      $('#rewardBtn').addClass('btn btn-info disabled')
+      
+      console.log('HEEE')
+});  
+
+
+
 //end    
 });    
 
@@ -33,3 +58,4 @@ var autocomplete = new google.maps.places.Autocomplete(input);
 
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
